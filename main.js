effeb877d3a7d72f397662d932293e2af638f7f1
“avery710” <angellinbalabababa@gmail.com>
2022-09-05 18:19:03 +0800

    Q35

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const btns = document.querySelectorAll(".button");
+const sections = document.querySelectorAll(".Q-section");
 
-btns.forEach(btn => {
+sections.forEach(section => {
+    const btn = section.querySelector(".button");
+    
     btn.addEventListener('click', (e) => {
         btn.classList.toggle("active");
+
+        const answer = btn.nextElementSibling;
+
+        if (btn.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+            section.style.minHeight = answer.scrollHeight + 30 + 'px';
+        }
+        else {
+            answer.style.maxHeight = 0;
+            section.style.minHeight = '100px';
+        }
     });
 });
